6cb0d9a561bfc5a6510c273ff4945103f7031f08
Mansoor Roeen <drunkenpirate47x@gmail.com>
2022-02-08 19:36:05 +0530

    changes have been made

---
+++ src/components/popup/ConfirmAlert.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function ConfirmAlert({...props}) {
+
+    // Passed Props
+
+    const {
+        title,
+        messages,
+        buttons,
+        abortFunc,
+        confirmFunc,
+        onClose,
+    } = props
+
+    
+    // React States
+
+    const [--]
+
+    return (
+        <Wrapper>
+        </Wrapper>
+    );
+
+}
